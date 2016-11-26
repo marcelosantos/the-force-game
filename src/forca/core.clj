6ebaff2b0f42ad6cda2c71e94781b882a3e18151
Marcelo Santos <marcelosantosadm@gmail.com>
2016-11-26 21:08:09 +0000

    implement new changes

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,51 +1,51 @@
 (ns forca.core
     (:gen-class))
 
-(def total-de-vidas 6)
-(def palavra-secreta "CLOJURE")
+(def total-of-lifes 6)
+(def secret-word "CLOJURE")
 
-(defn perdeu [] (print "Você perdeu!!! "))
-(defn ganhou [] (print "Você ganhou!!! "))
+(defn you-lose [] (print "Você perdeu!!! "))
+(defn you-win [] (print "Você ganhou!!! "))
 
-(defn letras-faltantes [palavra acertos]
-	(remove (fn [letra] (contains? acertos (str letra))) palavra))
+(defn missing-letters [the-word the-hits]
+	(remove (fn [the-letter] (contains? the-hits (str the-letter))) the-word))
 
-(defn acertou-a-palavra-toda? [palavra acertos]
-	(empty? (letras-faltantes palavra acertos)))
+(defn hit-the-all-word? [the-word the-hits]
+	(empty? (missing-letters the-word the-hits)))
 
-(defn le-letra! [] (read-line))
+(defn read-letter! [] (read-line))
 
-(defn acertou? [chute palavra] (.contains palavra chute))
+(defn has-hit? [the-shot the-word] (.contains the-word the-shot))
 
-(defn imprime-forca [vidas palavra acertos]
-	(println "Vidas " vidas)
-	(doseq [letra (seq palavra)]
-		(if (contains? acertos (str letra))
-		(print letra " ")
+(defn print-the-force [the-lifes the-word the-hits]
+	(println "Vidas " the-lifes)
+	(doseq [the-letter (seq the-word)]
+		(if (contains? the-hits (str the-letter))
+		(print the-letter " ")
 		(print "_" " ")))
 	(println))
 
-(defn jogo [vidas palavra acertos]
-	(imprime-forca vidas palavra acertos)
+(defn the-game [the-lifes the-word the-hits]
+	(print-the-force the-lifes the-word the-hits)
 	(cond
-		(= vidas 0) (perdeu)
-		(acertou-a-palavra-toda? palavra acertos) (ganhou)
+		(= the-lifes 0) (you-lose)
+		(hit-the-all-word? the-word the-hits) (you-win)
 		:else
-		(let [chute (le-letra!)]
-			(if (acertou? chute palavra)
+		(let [the-shot (read-letter!)]
+			(if (has-hit? the-shot the-word)
 				(do
 					(println "Acertou a letra! ")
-					(recur vidas palavra (conj acertos chute)))
+					(recur the-lifes the-word (conj the-hits the-shot)))
 				(do
 					(println "Errou a letra! ")
-					(recur (dec vidas) palavra acertos))))))
+					(recur (dec the-lifes) the-word the-hits))))))
 
-(defn avalia-chute [chute vidas palavra acertos]
-	(if (acertou? chute palavra)
-		(jogo vidas palavra (conj acertos chute))
-		(jogo (dec vidas) palavra acertos)))
+(defn avalia-the-shot [the-shot the-lifes the-word the-hits]
+	(if (has-hit? the-shot the-word)
+		(the-game the-lifes the-word (conj the-hits the-shot))
+		(the-game (dec the-lifes) the-word the-hits)))
 
-(defn comeca-o-jogo [] (jogo total-de-vidas palavra-secreta #{}))
+(defn start-the-game [] (the-game total-of-lifes secret-word #{}))
 
 (defn -main [& args]
-    (comeca-o-jogo))
+    (start-the-game))
