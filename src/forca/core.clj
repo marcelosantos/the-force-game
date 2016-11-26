d47679ee682efbb5bba08ce336266e99e6a81515
Marcelo Santos <marcelosantosadm@gmail.com>
2016-11-26 20:32:15 +0000

    implementing main method on the program

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -2,41 +2,50 @@
   (:gen-class))
 
 (def total-de-vidas 6)
+(def palavra-secreta "MELANCIA")
 
 (defn perdeu [] (print "Você perdeu!!! "))
-(defn ganhou [] (print "Você ganhou!!!"))
+(defn ganhou [] (print "Você ganhou!!! "))
 
 (defn letras-faltantes [palavra acertos]
-	(remove (fn [letra] (contains? acertos (str letra))) palavra)
-)
+	(remove (fn [letra] (contains? acertos (str letra))) palavra))
 
 (defn acertou-a-palavra-toda? [palavra acertos]
-	(empty? (letras-faltantes palavra acertos))
-)
+	(empty? (letras-faltantes palavra acertos)))
 
 (defn le-letra! [] (read-line))
 
 (defn acertou? [chute palavra] (.contains palavra chute))
 
+(defn imprime-forca [vidas palavra acertos]
+	(println "Vidas " vidas)
+	(doseq [letra (seq palavra)]
+		(if (contains? acertos (str letra))
+		(print letra " ")
+		(print "_" " ")))
+	(println))
+
 (defn jogo [vidas palavra acertos]
-	(if (= vidas 0)
-		(perdeu)
-		(if (acertou-a-palavra-toda? palavra acertos)
-			(ganhou)
-			(avalia-chute (le-letra!) vidas palavra acertos)
-		)
-	)
-)
+	(imprime-forca vidas palavra acertos)
+	(cond
+		(= vidas 0) (perdeu)
+		(acertou-a-palavra-toda? palavra acertos) (ganhou)
+		:else
+		(let [chute (le-letra!)]
+			(if (acertou? chute palavra)
+				(do
+					(println "Acertou a letra! ")
+					(recur vidas palavra (conj acertos chute)))
+				(do
+					(println "Errou a letra! ")
+					(recur (dec vidas) palavra acertos))))))
 
 (defn avalia-chute [chute vidas palavra acertos]
 	(if (acertou? chute palavra)
 		(jogo vidas palavra (conj acertos chute))
-		(jogo (dec vidas) palavra acertos)
-	)
-)
+		(jogo (dec vidas) palavra acertos)))
 
+(defn comeca-o-jogo [] (jogo total-de-vidas palavra-secreta #{}))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (comeca-o-jogo))
