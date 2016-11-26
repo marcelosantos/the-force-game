3ed9b69c76fa86d041863bcc3d57ffb42ed0cb86
Marcelo Santos <marcelosantosadm@gmail.com>
2016-11-26 14:38:26 +0000

    implementing change about checking letter with console input read

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -14,15 +14,27 @@
 	(empty? (letras-faltantes palavra acertos))
 )
 
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
 (defn jogo [vidas palavra acertos]
 	(if (= vidas 0)
 		(perdeu)
 		(if (acertou-a-palavra-toda? palavra acertos)
 			(ganhou)
-			(print "Chuta, amigo!")
+			(avalia-chute (le-letra!) vidas palavra acertos)
 		)
 	)
 )
+
+(defn avalia-chute [chute vidas palavra acertos]
+	(if (acertou? chute palavra)
+		(jogo vidas palavra (conj acertos chute))
+		(jogo (dec vidas) palavra acertos)
+	)
+)
+
 
 (defn -main
   "I don't do a whole lot ... yet."
